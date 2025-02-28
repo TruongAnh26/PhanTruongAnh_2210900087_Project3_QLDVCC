8be5adbda825365ddc878dc8fa48205b1c144a70
Bien Luong <bienc.luong@dssolution.jp>
2025-02-28 20:40:37 +0700

    build base fe and be

---
+++ pta-backend/src/test/java/net/javaspring/pta_backend/PtaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaspring.pta_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
